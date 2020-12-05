42d56b7b0fa96f3828cba6c909f46f8c46ee7991
X-Cyborg <puneethk.cs18@sahyadri.edu.in>
2020-12-05 10:34:40 +0530

    Create UnionandIntersection.cpp

---
+++ Array/UnionandIntersection.cpp
@@ -0,0 +1,77 @@
+// Union and Intersection of an Array
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int printUnion(int arr1[], int arr2[],int m,int n)
+{
+    int i=0,j=0;
+    while(i<m && j<n)
+     {
+        if(arr1[i] < arr2[j])
+        { 
+           printf("%d",arr1[i++]);
+         }
+         else if(arr2[j] < arr1[i])
+        { 
+           printf("%d",arr2[j++]);
+         }
+         else
+           printf("%d",arr2[j++]);
+           i++;
+     }
+     while(i<m)
+       {
+          printf("%d",arr1[i++]);
+        }
+     while(j<n)
+      {
+         printf("%d",arr2[j++]);
+      }
+}
+
+int main()
+ {
+   int arr1[] = {1,2,3,4,5,6};
+   int arr2[]= {3,4,5,6};
+   int m = sizeof(arr1) / sizeof(arr1[0]); 
+   int n = sizeof(arr2) / sizeof(arr2[0]);
+   printUnion(arr1, arr2, m, n); 
+    return 0; 
+} 
+
+
+2. Intersection of an array
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int printIntersection(int arr1[],int arr2[], int m,int n)
+{
+    int i=0,j=0;
+    while(i<m && j<n)
+    { 
+       if(arr1[i] < arr2[j])
+         i++;
+       else if(arr2[j] < arr1[i])
+         j++;
+       else
+         cout<<arr2[j]<<" ";
+         i++;j++;     
+    }
+}    
+
+
+int main()
+ {
+   int arr1[] = {1,2,3,4,5,6};
+   int arr2[]= {3,4,5,6};
+   int m = sizeof(arr1) / sizeof(arr1[0]); 
+   int n = sizeof(arr2) / sizeof(arr2[0]);
+   printIntersection(arr1, arr2, m, n); 
+    return 0; 
+} 
+
+   
+         
+           
